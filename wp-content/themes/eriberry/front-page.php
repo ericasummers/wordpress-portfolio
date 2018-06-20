afa3b47635c95f9b584c6b684d2b336ff6678428
Erica Wright <erica.wright@soliddigital.com>
2018-06-19 22:52:22 -0700

    Update front page display

--- wp-content/themes/eriberry/front-page.php
+++ wp-content/themes/eriberry/front-page.php
@@ -4,10 +4,6 @@ class Front_Page extends Core_Template {
 	public function fields() {
 
 		$post = get_post($this->obj_id);
-		$post->custom_fields = core_get_fields( $this->obj_id, '', array(
-			'hello'
-		) );
-
 		return $post;
 
 	}
