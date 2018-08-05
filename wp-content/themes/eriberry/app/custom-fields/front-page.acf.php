e19428880163942cc78e62f202d68328270fa0ae
Erica Wright <erica.wright@soliddigital.com>
2018-08-05 14:37:24 -0700

    Add CTA fields to front page

--- wp-content/themes/eriberry/app/custom-fields/front-page.acf.php
+++ wp-content/themes/eriberry/app/custom-fields/front-page.acf.php
@@ -11,7 +11,12 @@ $fields = [
 
 	[ 'tab', 'Content & Image', ['placement' => 'left'] ],
 	[ 'textarea', 'Text Content' ],
-	[ 'image', 'Optional Image', ['return_format' => 'array'] ]
+	[ 'image', 'Optional Image', ['return_format' => 'array'] ],
+
+	[ 'tab', 'Call to Action', ['placement' => 'left'] ],
+	[ 'text', 'CTA Title' ],
+	[ 'text', 'Optional CTA Description' ],
+	[ 'link', 'CTA Link', ['return_format' => 'array'] ]
 ];
 
 $field_group = core_register_field_group( 'homepage-acf', $group_args, $fields );
