3251a4f90abf2f474ad58071633198e68154df13
Erica Wright <erica.wright@soliddigital.com>
2018-08-05 14:17:03 -0700

    Add custom fields to front page

--- wp-content/themes/eriberry/app/custom-fields/front-page.acf.php
+++ wp-content/themes/eriberry/app/custom-fields/front-page.acf.php
@@ -6,7 +6,12 @@ $group_args = [
 ];
 
 $fields = [
-	[ 'text', 'Hello' ]
+	[ 'tab', 'Full Width Content', ['placement' => 'left'] ],
+	[ 'textarea', 'Optional Full Width Content' ],
+
+	[ 'tab', 'Content & Image', ['placement' => 'left'] ],
+	[ 'textarea', 'Text Content' ],
+	[ 'image', 'Optional Image', ['return_format' => 'array'] ]
 ];
 
 $field_group = core_register_field_group( 'homepage-acf', $group_args, $fields );
