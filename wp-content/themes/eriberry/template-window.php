457e385b129f6f357e7883028679d2152e996f82
Erica Wright <erica.wright@soliddigital.com>
2018-08-10 19:05:32 -0700

    Update CTA display section and add fields to window template

--- wp-content/themes/eriberry/template-window.php
+++ wp-content/themes/eriberry/template-window.php
@@ -6,12 +6,18 @@
 class Template_Window extends Core_Template {
 
     public function template_file() {
-		return 'views/template-window.twig';
+			return 'views/template-window.twig';
     }
 
     public function fields() {
-		$post = get_post($this->obj_id);
-		return $post;
+			
+			$fields = core_get_fields($this->obj_id, '', array(
+				'cta_title',
+				'optional_cta_description',
+				'cta_link'
+			));
+			$fields['post'] = get_post($this->obj_id);
+			return $fields;
 	}
 
 }
